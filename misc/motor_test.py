194131a91bc75146527eba2310bb50a6aa3a9898
Daniel Martinez <danmartinez78@gmail.com>
2016-07-09 12:38:08 -0500

    motor tuning

--- misc/motor_test.py
+++ misc/motor_test.py
@@ -29,3 +29,30 @@ motors.driveMotor('both', 'fwd', 50)
 time.sleep(2)
 print("seven")
 motors.driveMotor('both', 'fwd', 0)
+
+print("one")
+motors.driveMotor('left', 'rev', 50)
+
+time.sleep(2)
+print("two")
+motors.driveMotor('right', 'fwd', 50)
+
+time.sleep(2)
+print("three")
+motors.driveMotor('both', 'fwd', 10)
+
+time.sleep(2)
+print("four")
+motors.driveMotor('left', 'rev', 10)
+
+time.sleep(2)
+print("five")
+motors.driveMotor('right', 'rev', 10)
+
+time.sleep(2)
+print("six")
+motors.driveMotor('both', 'fwd', 50)
+
+time.sleep(2)
+print("seven")
+motors.driveMotor('both', 'fwd', 0)
--- src/robot_control/src/RobotControl.py
+++ src/robot_control/src/RobotControl.py
@@ -31,7 +31,6 @@ class RobotControl(object):
         self.ros_interface = ROSInterface(t_cam_to_body)
 
         # YOUR CODE AFTER THIS
-        
         # Uncomment as completed
         #self.kalman_filter = KalmanFilter(world_map)
         #self.diff_drive_controller = DiffDriveController(max_speed, max_omega)
@@ -43,8 +42,12 @@ class RobotControl(object):
         """
         meas = self.ros_interface.get_measurements()
         imu_meas = self.ros_interface.get_imu()
-        self.ros_interface.command_velocity(0.0, 5.0)
-        
+        #time = rospy.get_time()
+        #while True:
+            #if (rospy.get_time() - time > 1):
+                #self.ros_interface.command_velocity(0.0, 0.0)
+            #else:
+                #self.ros_interface.command_velocity(0.3, 0.0)
         return
     
 def main(args):
