5aec8fe84e45f3ca935dacf9c15a3a1422f54f6e
Daniel Martinez <danmartinez78@gmail.com>
2016-08-28 21:14:24 -0500

    submitted

---
+++ src/robot_control/src/KalmanFilter.py
@@ -0,0 +1,177 @@
+#!/usr/bin/python
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+from matplotlib import patches
+# import pylab
+import time
+import math
+from numpy.linalg import inv
+
+
+class KalmanFilter:
+    """
+    Class to keep track of the estimate of the robots current state using the
+    Kalman Filter
+    """
+
+    def __init__(self, markers):
+        """
+        Initialize all necessary components for Kalman Filter, using the
+        markers (AprilTags) as the map
+        Input: 
+        markers - an N by 4 array loaded from the parameters, with each element
+            consisting of (x,y,theta,id) where x,y gives the 2D position of a
+            marker/AprilTag, theta gives its orientation, and id gives its
+            unique id to identify which one you are seeing at any given
+            moment
+        """
+        self.markers = markers
+        self.last_time = 0  # None # Used to keep track of time between measurements
+        self.Q_t = np.eye(3) * 1000
+        self.R_t = np.eye(3) * 100
+
+        # YOUR CODE HERE
+        self.x_t = np.array([0, 0, 0])
+        self.x = 0
+        self.y = 0
+        self.theta = 0
+        self.dx = 0
+        self.dy = 0
+        self.dtheta = 0
+        self.dt = 1.0/60.0
+        self.z_t = np.array([0, 0, 0])
+        self.P_t = np.eye(3) * 1000000
+        #print self.P_t
+        self.n = np.array([0, 0, 0])
+        #print self.n
+
+    def step_filter(self, v=None, imu_meas=None, z_t=None):
+        """
+        Perform step in filter, called every iteration (on robot, at 60Hz)
+        Inputs:
+        v, imu_meas - descriptions in prediction. Will be None value if
+            values are not available
+        z_t - description in update. Will be None value if measurement is not
+            available
+        Outputs:
+        x_t - current estimate of the state
+        """
+        # YOUR CODE HERE
+        if v!=None and imu_meas!= None:
+            #print "PREDICTION"
+            self.prediction(v, imu_meas)
+           # print self.x_t
+           # print self.P_t
+        else:
+            self.dx = 0
+            self.dy = 0
+            self.dtheta = 0
+            
+        if z_t != None and z_t != []:
+             #print "UPDATE"
+	     self.update(z_t)
+        return self.x_t
+
+    def prediction(self, v, imu_meas):
+        """
+        Performs the prediction step on the state x_t and covariance P_t
+        Inputs:
+        v - a number representing in m/s the commanded speed of the robot
+        imu_meas - a 5 by 1 numpy array consistening of the values
+            (acc_x,acc_y,acc_z,omega,time), with the fourth of the values giving
+            the gyroscope measurement for angular velocity (which you should
+            use as ground truth) and time giving the current timestamp. Ignore
+            the first three values (they are for the linear acceleration which
+            we don't use)
+        Outputs: a tuple with two elements
+        predicted_state - a 3 by 1 numpy array of the predction of the state
+        predicted_covariance - a 3 by 3 numpy array of the predction of the
+            covariance
+        """
+        # YOUR CODE HERE
+        # propagate pose x,y,theta
+        # print self.x_t
+        #print v
+        self.dt = 1.0/60.0
+        self.last_time = imu_meas[4]
+        #print self.dt
+        self.dtheta = float(imu_meas[3])*self.dt
+        self.theta += self.dtheta
+        #print(v*math.cos(self.theta))*self.dt
+        self.dx = (v * math.cos(self.theta) * self.dt)
+        self.dy = (v * math.sin(self.theta) * self.dt)
+        #print self.dx,self.dy,self.dtheta
+        self.x += self.dx
+        self.y += self.dy
+        self.x_t = np.array([self.x, self.y, self.theta])  # probably should fix this
+        # print self.x_t
+        # propagate covariance
+        dfdx = np.eye(3) + np.array([[0, 0, -self.dy], [0, 0, self.dx], [0, 0, 0]])
+        dfdxt = dfdx.transpose()
+        # dfdn = np.array([[-math.sin(self.theta)*dt, 0, 0],[math.cos(self.theta)*dt, 0, 1]])
+        self.P_t = dfdx * self.P_t * dfdxt
+        self.P_t += self.Q_t
+        pass
+
+    def update(self, z_t):
+        """
+        Performs the update step on the state x_t and covariance P_t
+        Inputs:
+        z_t - an array of length N with elements that are 4 by 1 numpy arrays.
+            Each element has the same form as the markers, (x,y,theta,id), with
+            x,y gives the 2D position of the measurement with respect to the
+            robot, theta the orientation of the marker with respect to the
+            robot, and the unique id of the marker, which you can find the
+            corresponding marker from your map
+        Outputs:
+        predicted_state - a 3 by 1 numpy array of the updated state
+        predicted_covariance - a 3 by 3 numpy array of the updated covariance
+        """
+        # YOUR CODE HERE
+        # print(z_t)
+        # iterate through detected tags
+        #print z_t
+        for tag in z_t:
+            # for each tag -> find world frame coords from self.markers
+            xr = tag[0]
+            yr = tag[1]
+            tr = tag[2]
+            for ref in self.markers:
+                if tag[3] == ref[3]:
+                    xw = ref[0]
+                    yw = ref[1]
+                    tw = ref[2]
+                    # with body frame & world frame coords of tag -> find world frame coords of robot
+                    x = xw - xr*math.cos(tr-tw) - yr*math.sin(tr-tw)
+                    y = yw - yr*math.cos(tr-tw) + xr*math.sin(tr-tw)
+                    theta = math.atan2(-math.sin(tr-tw), math.cos(tr-tw))
+                    x += self.dx
+                    y += self.dy
+                    theta += self.dtheta
+                    # theta = theta % (2 * math.pi)
+                    self.z_t = np.array([x, y, theta])
+                    #print "observation"
+                    #print self.z_t, tag[3]
+                    # calc kalman gain
+                    #print "gain"
+                    dhdx = np.eye(3)
+                    dhdxt = dhdx.transpose()
+                    c = dhdx * self.P_t * dhdxt + self.R_t
+                    k = self.P_t * dhdxt * inv(c)
+                    #print k
+                    # combine observation w/ prediction
+                    #print "priori"
+                    #print self.x_t
+                    #print self.P_t
+                    #print "postori"
+                    self.x += k[0,0] * (x - self.x)
+                    self.y += k[1,1] * (y - self.y)
+                    self.theta += k[2,2] * (theta - self.theta)
+                    self.x_t = np.array([self.x, self.y, self.theta])
+                    #print self.x_t
+                    # update covariance
+                    self.P_t = self.P_t*(np.eye(3) - k*dhdx)
+                    #print self.P_t
+            self.P_t += np.eye(3) * 100   
+        pass
---
+++ src/robot_control/src/ShortestPath.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python
+
+import numpy as np
+import yaml
+
+def dijkstras(occupancy_map,x_spacing,y_spacing,start,goal):
+    """
+    Implements Dijkstra's shortest path algorithm
+    Input:
+    occupancy_map - an N by M numpy array of boolean values (represented
+        as integers 0 and 1) that represents the locations of the obstacles
+        in the world
+    x_spacing - parameter representing spacing between adjacent columns
+    y_spacing - parameter representing spacing between adjacent rows
+    start - a 3 by 1 numpy array of (x,y,theta) for the starting position 
+    goal - a 3 by 1 numpy array of (x,y,theta) for the finishing position 
+    Output: 
+    path: list of the indices of the nodes on the shortest path found
+        starting with "start" and ending with "end" (each node is in
+        metric coordinates)
+    """
+    # Your  code  here
+    [nrows,ncols] = occupancy_map.shape
+    mymap = np.zeros((nrows,ncols))
+
+    distanceFromStart = np.zeros((nrows,ncols))
+    distanceFromStart = distanceFromStart + 100000000
+    ##print(distanceFromStart)
+    mymap[occupancy_map==0] = 1
+    mymap[occupancy_map==1] = 2
+    ##print(mymap)
+
+    start_node_i = round((start[1] / y_spacing) - 0.5)+0.25
+    start_node_j = round((start[0] / x_spacing) - 0.5)
+    dest_node_i = round((goal[1] / y_spacing) - 0.5)
+    dest_node_j = round((goal[0] / x_spacing) - 0.5)
+
+    mymap[(start_node_i, start_node_j)] = 5
+    mymap[(dest_node_i, dest_node_j)] = 6
+    distanceFromStart[(start_node_i, start_node_j)] = 0
+
+    ##print(distanceFromStart)
+    ##print(occupancy_map)
+    ##print(mymap)
+
+    parent = np.zeros((nrows, ncols))
+    numExpanded = 0
+    [imax, jmax] = occupancy_map.shape
+    imax = imax-1
+    jmax = jmax-1
+    imin = 0
+    jmin = 0
+    delta_i = [1, -1, 0, 0]
+    delta_j = [0, 0, 1, -1]
+    edge = [y_spacing, y_spacing, x_spacing, x_spacing]
+
+    while (True):
+        min_dist = distanceFromStart.min()
+        current = distanceFromStart.argmin()
+        i,j = np.unravel_index(distanceFromStart.argmin(), distanceFromStart.shape)
+        ##print(i, j)
+        ##print(min_dist)
+        #test = ravel_index(current, distanceFromStart.shape)
+        if (i==dest_node_i and j==dest_node_j) or (distanceFromStart[(i, j)] > 1000000):
+            #print("DONE")
+            break
+        mymap[(i,j)] = 3
+        distanceFromStart[(i, j)] = 100000000
+        for index in range(0, len(delta_i)):
+            if (i + delta_i[index] <= imax) and (i + delta_i[index] >= imin) and (j + delta_j[index] <= jmax) and (j + delta_j[index] >= jmin):
+                if (mymap[i + delta_i[index], j + delta_j[index]] != 3 and mymap[i+delta_i[index], j+delta_j[index]] != 4 and mymap[i+delta_i[index], j+delta_j[index]] != 5 and mymap[i+delta_i[index], j+delta_j[index]] != 2):
+                    if (distanceFromStart[i + delta_i[index], j + delta_j[index]] > min_dist + edge[index]):
+                        distanceFromStart[i + delta_i[index], j + delta_j[index]] = min_dist + edge[index]
+                        parent[i + delta_i[index], j + delta_j[index]] = current
+                    mymap[i + delta_i[index], j + delta_j[index]] = 4
+        numExpanded = numExpanded + 1
+
+    #print("Finding Route")
+    #print(parent)
+    if (distanceFromStart[(dest_node_i, dest_node_j)]>1000):
+        route = np.array
+        #print("No Path")
+        return(route)
+    else:
+        route = np.array([(dest_node_i, dest_node_j)])
+        while (parent[(route[0,0], route[0,1])] != 0):
+            loc = int(parent[(route[0,0], route[0,1])])
+            data_shape = occupancy_map.shape
+            [pi,pj] = np.unravel_index(loc, data_shape)
+            new_parents = np.array([(pi,pj)])
+            route = np.vstack((new_parents, route))
+    #print(route)
+    [rows, cols] = route.shape
+    routexy = np.zeros((rows,cols))
+    for i in range(0, rows):
+        routexy[(i, 0)] = (route[(i, 1)] + 0.5) * x_spacing
+        routexy[(i, 1)] = (route[(i, 0)] + 0.5) * y_spacing
+    routexy = np.vstack(([(start[0], start[1])], routexy))
+    #print(routexy)
+    return(routexy)
+    pass
+
+def main():
+    # Load parameters from yaml
+    param_path = 'params.yaml' # rospy.get_param("~param_path")
+    f = open(param_path,'r')
+    params_raw = f.read()
+    f.close()
+    params = yaml.load(params_raw)
+    # Get params we need
+    occupancy_map = np.array(params['occupancy_map'])
+    pos_init = np.array(params['pos_init'])
+    pos_goal = np.array(params['pos_goal'])
+    x_spacing = params['x_spacing']
+    y_spacing = params['y_spacing']
+    path = dijkstras(occupancy_map,x_spacing,y_spacing,pos_init,pos_goal)
+    #print(path)
+    return path
+
+if __name__ == '__main__':
+    main()
+
